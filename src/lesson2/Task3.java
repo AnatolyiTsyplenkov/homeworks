29e10b54b6f80ec22ed6173f9043e02aab86eef8
AnatolyiTsyplenkov <av_tsyplenkov@mail.ru>
2020-11-28 16:25:47 +0300

    Merge remote-tracking branch 'origin/HomeworkForLeson1' into HomeworkForLeson1

---
+++ src/lesson2/Task3.java
@@ -0,0 +1,26 @@
+package lesson2;
+
+import java.util.Arrays;
+
+public class Task3 {
+
+    //3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом,
+    // и числа меньшие 6 умножить на 2;
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+
+        System.out.println(Arrays.toString(arr));
+
+        for (int i = 0; i < arr.length; i++) {
+
+            arr[i] = arr[i] < 6 ? arr[i] * 2 : arr[i];
+
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+}
